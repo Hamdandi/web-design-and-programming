c3a87057f646d5b6c8ada246526a0b5bf60dd5e3
Hamdandi <90332110+Hamdandi@users.noreply.github.com>
2022-04-14 09:21:41 +0700

    Create index.php
    belajar PHP

---
+++ ppw13/index.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>belajar PHP</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+<body>
+<div class = "container-fluid">
+<?php
+    // komen 1 baris
+    /*
+    komen multi line
+    */
+
+    // cara menampilkan pesan di php
+    echo "selamat pagi";
+    print "<p>hari ini belajar php dasar</p>";
+
+    // ========== variable START ==========
+    $npm            = 123456789;
+    $nama           = "kiki";
+    $tempat_lahir   = "palembang";
+    $tanggal_lahir  = "2000-13-15";
+
+    // array index di awali index 0
+    $hobi           = ["game", "ngoding", "jalan-jalan"];
+    $skill          = array ("JS", "CSS", "HTML");
+
+    // array asosiatif (key => value)
+    $nilai  = [
+        "S001" => "olahraga",
+        "nilai" => "A"
+    ];
+
+    // Array multidimensi
+    $transkrip  = [
+        [
+            "kode_mk" => "S001",
+            "nama_mk" => "olahraga",
+            "sks"     => 2,
+            "nilai"   => "A"
+        ],
+        [
+            "kode_mk" => "S002",
+            "nama_mk" => "olahraga",
+            "sks"     => 2,
+            "nilai"   => "B"
+        ]
+        ];
+
+    // ========== variable END ==========
+
+    echo "nama saya           : ". $nama;
+    echo "<br>npm             : ". $npm;
+    echo "<br>tempat lahir    : ". $tempat_lahir;
+    echo "<br>tanggal lahir   : ". $tanggal_lahir;
+    echo "<br>hobi saya       : ". $hobi[0];
+    echo "<br>skill saya      : ". $skill[0];
+    echo "<br>pelajaran       : ". $nilai["S001"];
+    echo "<br>nilai           : ". $nilai["nilai"];
+    echo "<hr>"; 
+
+    echo "<ul>";
+    foreach ($hobi as $row) {
+        echo "<li>" .$row. "</li>";
+    };
+    echo "</ul>";
+
+    echo "<table class = 'table table-dark table-striped'> 
+    <thead>
+        <tr>
+            <th>Kode MK</th>
+            <th>Nama MK</th>
+            <th>SKS</th>
+            <th>Nilai</th>
+            <th>Bobot</th>
+            <th>KxN</th>
+        </tr>
+    </thead>
+    <tbody>";
+    foreach ($transkrip as $row) {
+        echo "<tr>";
+        echo "<td>". $row ["kode_mk"]. "</td>";
+        echo "<td>". $row ["nama_mk"]. "</td>";
+        echo "<td>". $row ["sks"]. "</td>";
+        echo "<td>". $row ["nilai"]. "</td>";
+        echo "<td>". get_bobot($row ["nilai"]). "</td>";
+        echo "</tr>";
+    };
+    echo "</table>";
+
+    function get_bobot($nilai) {
+        $array_nilai = [
+            "A"     => 4,
+            "A-"    => 3.7,
+            "B"     => 3,
+        ];
+        return $array_nilai[$nilai];
+    };
+
+?>
+</div>
+</body>
+</html>
